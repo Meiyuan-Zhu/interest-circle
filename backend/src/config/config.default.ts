53dd1c6995d65aed28468c79ce52d08f76460274
竺梅缘 <zhumeiyuan@zhumeiyuandeair.home>
2024-08-01 18:04:24 -0400

    backend JWT&config

--- backend/src/config/config.default.ts
+++ backend/src/config/config.default.ts
@@ -1,9 +1,23 @@
 import { MidwayConfig } from '@midwayjs/core';
+import { join } from 'path';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: join(__dirname,'../../.env')})
 
 export default {
-  // use for cookie sign key, should change to your own and keep security
-  keys: '1722517303550_5633',
+  keys: 'O0vyCN9yemfxobhZkBtPM/HfTM6y6Mxazs8vTzhbuE8=',
   koa: {
     port: 7001,
   },
+  jwt: {
+    secret: process.env.JWT_SECRET,
+  },
+  typeorm: {
+    type: "sqlite",
+    database: join(__dirname, '../../database/sqlite'),
+    synchronize: true,
+    logging:false,
+    entities: [join(__dirname, '../models/*.ts')],
+
+  }
 } as MidwayConfig;
