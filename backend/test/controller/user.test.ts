a9eb798d3f86f980eaeac1106ac4225c1b45da14
竺梅缘 <zhumeiyuan@zhumeiyuandeair.home>
2024-08-01 19:05:18 -0400

    backend- register and login

---
+++ backend/test/controller/user.test.ts
@@ -0,0 +1,40 @@
+import { createApp, close, createHttpRequest } from '@midwayjs/mock';
+import { Framework } from '@midwayjs/koa';
+import * as assert from 'assert';
+import { after } from 'node:test';
+
+describe('UserController', ()=> {
+    let app;
+    beforeAll(async () => {
+        // create app
+        app = await createApp<Framework>('../../src');
+    });
+
+    afterAll(async () => {
+        // close app
+        await close(app);
+    });
+
+    it('should POST /api/users/register', async () => {
+        const result = await createHttpRequest(app).post('/api/users/register').send({
+            username: 'test',
+            password: '123456',
+        });
+        assert(result.status === 201);
+        assert(result.body.id);
+    
+    })
+
+    it('should POST /api/users/login',async () => {
+        await createHttpRequest(app)
+            .post('/api/users/register')
+            .send({username: 'test', password: '123456'});
+
+        const restult = await createHttpRequest(app)
+            .post('/api/users/login')
+            .send({username: 'test', password: '123456'});
+
+        assert(restult.status === 200);
+        assert(restult.body.token);
+    });
+});
