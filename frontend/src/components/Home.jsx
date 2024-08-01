2d835547066bf6ccf3ed368e69a5a87319b2b130
竺梅缘 <zhumeiyuan@zhumeiyuandeair.home>
2024-08-01 10:57:55 -0400

    Backend CI configuration

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Home () {
+    return (
+        <>
+            <div style={{textAlign: 'center', marginTop: '50px'}}>
+                <h1>Welcome to Interest Circle!</h1>
+                <p>Join us to explore various interest circles and share your own ideas!</p>
+            </div>
+        </>
+    )
+}
+
+export default Home;
